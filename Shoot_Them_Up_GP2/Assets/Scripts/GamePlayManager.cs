1b12cfa38f1a5ea59459b164b80244a5cfb09a93
unknown <tanneauthomas56@gmail.com>
2021-02-12 12:19:26 +0100

    level1_panelGO_Win_Pause

--- Shoot_Them_Up_GP2/Assets/Scripts/GamePlayManager.cs
+++ Shoot_Them_Up_GP2/Assets/Scripts/GamePlayManager.cs
@@ -7,17 +7,14 @@ using UnityEngine.UI;
 
 public class GamePlayManager : MonoBehaviour
 {
-<<<<<<< HEAD
 
     [SerializeField]
     private bool pause = false;
 
     public GameObject panePause;
-=======
     public static GamePlayManager Instance;
     public Text scoreText;
     public float score;
->>>>>>> c2b2f62c5e1d5e4957495873dc5b015139da14ec
     // Start is called before the first frame update
     private void Awake()
     {
@@ -38,7 +35,6 @@ public class GamePlayManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         if (pause == false)
         {
             Time.timeScale = 1;
@@ -61,9 +57,7 @@ public class GamePlayManager : MonoBehaviour
                 panePause.SetActive(true);
             }
         }
-=======
         scoreText.text = "Score : " + score;
->>>>>>> c2b2f62c5e1d5e4957495873dc5b015139da14ec
     }
     
      public void onClick_Retry()
